0246e668dcf4631fdb6cab624d41ce64aafa33f3
LeandroFraga99 <71570108+LeandroFraga99@users.noreply.github.com>
2023-01-06 14:19:26 -0300

    Projeto finalizado

---
+++ src/components/cronometer/index.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
+import Botao from "../Button";
+import Clock from "./clock";
+import style from "./cronometer.module.scss";
+
+interface Props {
+    selecionado: ITarefa | undefined
+    finalizarTarefa: () => void
+}
+
+export default function Cronometer({ selecionado, finalizarTarefa }: Props) {
+    const [tempo, setTempo] = useState<number>();
+
+    useEffect(() => {
+        if (selecionado?.tempo) {
+            setTempo(tempoParaSegundos(selecionado.tempo))
+        }
+    }, [selecionado]);
+
+    function regressiva(contador: number = 0) {
+        setTimeout(() => {
+            if(contador > 0){
+                setTempo(contador -1 );
+                return regressiva (contador - 1);
+            }
+            finalizarTarefa();
+        }, 1000)
+    }
+
+    return (
+        <div className={style.cronometro}>
+            <p className={style.titulo}>
+                Escolha um card e inicie o cronômetro!
+            </p>
+            <div className={style.relogioWrapper}>
+                <Clock tempo={tempo}/>
+            </div>
+            <Botao onClick={() => regressiva(tempo)}>
+                Começar
+            </Botao>
+        </div>
+    )
+}
